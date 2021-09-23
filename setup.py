a3409fde5124d5acf12d9cef3889bf25f5fc1add
Avinash905 <avinash.90527@gmail.com>
2021-09-23 22:17:35 +0530

    version updated

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME="Avinash905"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.1",
+    version="0.0.2",
     author=USER_NAME,
     author_email="avinash.90527@gmail.com",
     description="it is an implementation of perceptron",
